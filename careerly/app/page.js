e071ec840f39b79bc7d1f5a6f8eafb985e0ebe1d
syedabdulmuzammil <abdulmuzammil@devsed.com>
2024-06-08 15:34:31 +0530

    kk

--- careerly/app/page.js
+++ careerly/app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-white h-full w-full text-black">
-   <Padding>hello my name is anas </Padding>   
+      <Padding>hello my name is anas boyyy </Padding>
     </div>
   );
 }
