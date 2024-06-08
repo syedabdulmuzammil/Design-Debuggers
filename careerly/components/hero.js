85b59e4e149d44676712478c66a526d57b324121
syedabdulmuzammil <abdulmuzammil@devsed.com>
2024-06-08 20:45:53 +0530

    kk

--- careerly/components/hero.js
+++ careerly/components/hero.js
@@ -1,37 +1,162 @@
+"use client";
 import React from "react";
 import Padding from "./padding";
 import Image from "next/image";
+import { easeInOut, motion, useMotionValue } from "framer-motion";
 
 const Hero = () => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const x1 = useMotionValue(0);
+  const y1 = useMotionValue(0);
+
+  const handleMouseMove = (e) => {
+    x.set((e.clientX - window.innerWidth / 2) / 25);
+    y.set((e.clientY - window.innerHeight / 2) / 25);
+    x1.set((e.clientX - window.innerWidth / 2) / 55);
+    y1.set((e.clientY - window.innerHeight / 2) / 55);
+  };
   return (
     <div
-      className="bg-black h-[80vh] w-full flex items-center justify-center relative
+      className="bg-black h-[100vh] w-full flex items-center justify-center relative
     "
+      onMouseMove={handleMouseMove}
     >
-      <Padding>le
-        <div className="bg-black fx flex-col text-white  ">
-          <div className="text-[85px] text-[#AEAEAE] font-space ">
-            Unlock Your Dream
+      <Padding>
+        <div className=" bg-transparent flex flex-col text-white absolute top-[60%] -translate-y-1/2 left-[50%] z-[100] -translate-x-1/2 ">
+          <div className="text-[85px] font-medium text-[#AEAEAE] font-space max-h-fit overflow-hidden ">
+            <motion.div
+              initial={{
+                translateY: "300px",
+              }}
+              animate={{
+                translateY: "0px",
+              }}
+              transition={{
+                duration: 0.66,
+                ease: easeInOut,
+              }}
+            >
+              Unlock Your Dream
+            </motion.div>
           </div>
-          <div className="flex items-center gap-[10px]">
-            <div className="bg-[#FF8A66] font-overused font-medium leading-none w-fit h-fit text-[59px] px-[23px] py-[11px] rounded-full text-black ">
-              Career
-            </div>
-            <div className="text-[85px] text-[#AEAEAE] font-space ">
-              {" "}
-              with Careerly
-            </div>
+          <div className=" max-h-fit overflow-hidden ">
+            <motion.div
+              className="flex items-center gap-[10px]"
+              initial={{
+                translateY: "300px",
+              }}
+              animate={{
+                translateY: "0px",
+              }}
+              transition={{
+                duration: 0.66,
+                ease: easeInOut,
+              }}
+            >
+              <div className="bg-[#FF8A66] font-medium font-overused  leading-none w-fit h-fit text-[59px] px-[23px] py-[11px] rounded-full text-black ">
+                Career
+              </div>
+              <div className="text-[85px] font-medium text-[#AEAEAE] font-space ">
+                {" "}
+                with Careerly
+              </div>
+            </motion.div>
           </div>
         </div>
       </Padding>
-      <div className=" absolute top-[25%] left-[7%] h-[246px] w-[160px] ">
+      <motion.div
+        className=" absolute top-[20%] left-[7%] h-[246px] w-[160px] filter grayscale "
+        style={{ x: x, y: y }}
+        initial={{
+          top: "50%",
+          left: "50%",
+        }}
+        animate={{
+          top: "20%",
+          left: "7%",
+        }}
+        transition={{
+          duration: 0.66,
+          ease: easeInOut,
+        }}
+      >
         <Image
           height={500}
           width={500}
           className=" object-cover h-full w-full"
           src={"/images/one.png"}
         />
-      </div>
+      </motion.div>
+      <motion.div
+        className=" absolute top-[22%] right-[10%] h-[160px] w-[264px] filter grayscale rounded-3xl overflow-hidden "
+        style={{ x: x, y: y }}
+        initial={{
+          top: "50%",
+          right: "50%",
+        }}
+        animate={{
+          top: "22%",
+          right: "10%",
+        }}
+        transition={{
+          duration: 0.66,
+          ease: easeInOut,
+        }}
+      >
+        <Image
+          height={500}
+          width={500}
+          className=" object-cover h-full w-full"
+          src={"/images/two.png"}
+        />
+      </motion.div>
+      <motion.div
+        className=" absolute top-[65%] right-[7%] h-[246px] w-[160px] filter grayscale rounded-3xl overflow-hidden "
+        style={{ x: x, y: y }}
+        initial={{
+          top: "50%",
+          right: "50%",
+        }}
+        animate={{
+          top: "65%",
+          right: "7%",
+        }}
+        transition={{
+          duration: 0.66,
+          ease: easeInOut,
+        }}
+      >
+        <Image
+          height={500}
+          width={500}
+          className=" object-cover h-full w-full"
+          src={"/images/three.png"}
+        />
+      </motion.div>
+      <motion.div
+        className=" z-[10] absolute top-[65%] left-[15%] h-[160px] w-[264px] filter grayscale rounded-3xl overflow-hidden "
+        style={{ x: x, y: y }}
+        initial={{
+          top: "50%",
+          left: "50%",
+        }}
+        animate={{
+          top: "65%",
+          left: "15%",
+        }}
+        transition={{
+          duration: 0.66,
+          ease: easeInOut,
+        }}
+      >
+        <Image
+          height={500}
+          width={500}
+          className=" object-cover h-full w-full"
+          src={"/images/four.png"}
+        />
+      </motion.div>
     </div>
   );
 };
--- careerly/components/navbar.js
+++ careerly/components/navbar.js
@@ -1,27 +1,29 @@
+"use client";
 import React from "react";
 import Padding from "./padding";
 import Logo from "@/public/icons/logo";
 import Bar from "@/public/icons/bar";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <Padding className={" fixed bg-black top-0 left-0 w-full z-[999] "}>
       <div className=" min-w-full rounded-full bg-black  flex items-center justify-between px-5 py-5 ">
-        <div className="bg-white font-overused w-[15rem]  flex gap-8 justify-center items-center rounded-full text-[18px]  ">
-          <div>Culture</div>
-          <div>Work</div>
-          <div>Services</div>
-        </div>
+        <motion.div className="bg-white font-spacee w-fit font-medium  flex gap-8 justify-center items-center rounded-full text-[15px] px-[20px] py-[10px] overflow-hidden ">
+          <div className=" cursor-pointer ">Home</div>
+          <div className=" cursor-pointer ">Features</div>
+          <div className=" cursor-pointer ">About</div>
+        </motion.div>
         <div className=" absolute top-[50%] -translate-y-1/2 left-[50%] -translate-x-1/2 ">
           <Logo />
         </div>
 
-        <div className="bg-white flex rounded-full text-[18px] gap-3 items-center justify-center px-2 leading-none ">
-          <div>Sign in</div>
+        <motion.div className=" flex rounded-full text-[18px] gap-6 items-center px-2 leading-none ">
+          <div className=" text-[#FFFFFF] font-overused ">Sign in</div>
           <div>
             <Bar />
           </div>
-        </div>
+        </motion.div>
       </div>
     </Padding>
   );
