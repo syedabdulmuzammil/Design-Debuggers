6af8b1a743d29dbf1089f643c71a44abb4fd2146
syedabdulmuzammil <abdulmuzammil@devsed.com>
2024-06-09 14:55:42 +0530

    kk

--- careerly/components/alljobs.js
+++ careerly/components/alljobs.js
@@ -1,9 +1,12 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Newcard from "./newcard";
 import Marker from "@/public/icons/marker";
 import Eligiblecar from "./eleigiblecar";
 import No from "./no";
 import Search from "@/public/icons/search";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const Alljobs = () => {
   const data = [
@@ -38,7 +41,27 @@ const Alljobs = () => {
       Eligible: true,
       img: "/images/google.png",
     },
+    {
+      company: "Google",
+      role: "Full Stack developer",
+      time: "Full-Time",
+      how: "Remote",
+      location: "Saint Gobain Enterprices, Gachibowli, Hyderabad, Telangana.",
+      when: "Core",
+      Eligible: true,
+      img: "/images/google.png",
+    },
 
+    {
+      company: "NVIDIA",
+      role: "Software Engineer",
+      time: "Full-Time",
+      how: "Remote",
+      location: "Saint Gobain Enterprices, Gachibowli, Hyderabad, Telangana.",
+      when: "Core",
+      Eligible: false,
+      img: "/images/NVIDIA.png",
+    },
     {
       company: "NVIDIA",
       role: "Software Engineer",
@@ -72,95 +95,87 @@ const Alljobs = () => {
     },
   ];
 
+  const [activeTab, setActiveTab] = useState(1);
+
+  const changeTab = (tabNumber) => {
+    setActiveTab(tabNumber);
+  };
+
   return (
     <div>
       <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-4">Job Listings</h1>
-        <div className="grid grid-cols-4  gap-4">
-          {data.map((job, index) => (
+        <div className="flex w-full justify-between px-12">
+          <div
+            className={`flex w-max rounded-full  font-medium px-1 py-1 items-center border bg-[#333333] border-[#333333] 
+       `}
+            style={{ marginLeft: "-0.5rem" }}
+          >
             <div
-              key={index}
-              className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300"
+              className={clsx(
+                "  px-8 py-[0.5rem]   cursor-pointer  relative z-40 rounded-full  transition-all font-overused font-semibold text-[1rem] md:text-[0.9rem] duration-1000",
+                activeTab == 1 ? "text-[#191919]" : "text-[white]"
+              )}
+              onClick={() => changeTab(1)}
             >
-              <div className="flex items-center mb-4">
-                <img
-                  src={job.img}
-                  alt={`${job.company} logo`}
-                  className="w-12 h-12 rounded-full mr-4"
+              {activeTab == 1 && (
+                <motion.span
+                  layoutId="tabs"
+                  className={`  px-8 py-[0.5rem] absolute top-0 rounded-full left-0 w-full h-full -z-10 text-black  
+              bg-[white]    `}
                 />
-                <div>
-                  <h2 className="text-xl font-semibold">{job.company}</h2>
-                  <p className="text-gray-500">{job.role}</p>
-                </div>
-              </div>
-              <div className="mb-2">
-                <span className="font-semibold">Time:</span> {job.time}
-              </div>
-              <div className="mb-2">
-                <span className="font-semibold">How:</span> {job.how}
-              </div>
-              <div className="mb-2">
-                <span className="font-semibold">Location:</span> {job.location}
-              </div>
-              <div className="mb-2">
-                <span className="font-semibold">When:</span> {job.when}
-              </div>
-              <div className="mb-2">
-                <span className="font-semibold">Eligible:</span>{" "}
-                {job.Eligible ? "Yes" : "No"}
+              )}
+              <div className="flex">
+                <div>On Campus</div>
               </div>
             </div>
+
+            <div
+              className={clsx(
+                " px-8 py-[0.5rem]   cursor-pointer  relative z-40 rounded-full font-overused font-semibold  transition-all text-[1rem] md:text-[0.9rem] duration-1000",
+                activeTab == 2 ? "text-[#191919]" : "text-[white]"
+              )}
+              onClick={() => changeTab(2)}
+            >
+              {activeTab == 2 && (
+                <motion.span
+                  layoutId="tabs"
+                  className={`  border border-[#f0f0f0] px-8 py-[0.5rem] absolute top-0 rounded-full left-0 w-full h-full -z-10 text-white bg-[white]      `}
+                />
+              )}
+              <div className=" flex gap-1 items-center">
+                <div className="  ">Off Campus</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-[257px] h-[45px] rounded-[22px] border-[1px] bg-[#2A2929] border-[#333333] flex items-center gap-3 justify-center ">
+            <div>
+              <Search />
+            </div>
+            <div className="font-space text-[16px] font-medium text-[#FFFFFF] ">
+              Search for placements
+            </div>
+          </div>
+        </div>
+        <h1 className="text-3xl font-bold mb-4">Job Listings</h1>
+        <div className="grid grid-cols-4 gap-y-14  gap-4 pr-12">
+          {data.map((job, index) => (
+            <Newcard
+              img={job.img}
+              name={job.company}
+              skill={job.role}
+              button={job.time}
+              button1={job.how}
+              button2={job.when}
+              location={"Location"}
+              svg={<Marker />}
+              locate={job.location}
+              register={"Register"}
+              eligible={job.Eligible}
+            ></Newcard>
           ))}
         </div>
-        <div className="flex">
-          <Newcard
-            img={"/images/meta.png"}
-            name={"Facebook (Meta)"}
-            skill={"Front end developer"}
-            button={"Full-time"}
-            button1={"remote"}
-            button2={"Core"}
-            location={"Location"}
-            svg={<Marker />}
-            locate={
-              "Saint Gobain Enterprices, Gachibowli, Hyderabad, Telangana."
-            }
-            register={"Register"}
-          ></Newcard>
-          <Eligiblecar
-            img={"/images/meta.png"}
-            name={"Facebook (Meta)"}
-            skill={"Front end developer"}
-            button={"Full-time"}
-            button1={"remote"}
-            button2={"Core"}
-            location={"Location"}
-            svg={<Marker />}
-            locate={
-              "Saint Gobain Enterprices, Gachibowli, Hyderabad, Telangana."
-            }
-            svg1={<No />}
-            eligible={"Not Eligible"}
-          ></Eligiblecar>
-        </div>
-      </div>
-      <div className="flex w-full justify-between">
-      <div className="flex">
-        <div className="w-[257px] h-[45px] rounded-[22px] border-[1px] bg-[#4C4C4C] border-[#333333] ">
-          <div>On Campus </div>
-        </div>
-        <div className="w-[257px] h-[45px] rounded-[22px] border-[1px] bg-[#2A2929] border-[#333333]">
-          {" "}
-          <div>Of Campus</div>
-        </div>
-        </div>
-
-        <div className="w-[257px] h-[45px] rounded-[22px] border-[1px] bg-[#2A2929] border-[#333333] flex items-center gap-3 justify-center ">
-          <div>
-            <Search />
-          </div>
-          <div className="font-space text-[16px] font-medium text-[#FFFFFF] ">Search for placements</div>
-        </div>
+        <div className="flex"></div>
       </div>
     </div>
   );
--- careerly/components/newcard.js
+++ careerly/components/newcard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Padding from "./padding";
 import Image from "next/image";
+import No from "./no";
 
 const Newcard = ({
   img,
@@ -12,58 +13,70 @@ const Newcard = ({
   location,
   svg,
   locate,
-  register,
+  eligible,
 }) => {
   return (
     <div>
       <Padding>
-      <div className="border-[1px] rounded-[20px] bg-[#2A2929] border-[#333333] w-fit px-5 py-5 ">
-        <div className="flex items-center gap-3 py-5">
-          <div className="w-[48px] h-[48px] rounded-[10px] border-[1px] ">
-            <Image
-              height={500}
-              width={500}
-              className=" object-cover h-full w-full"
-              src={img}
-            />{" "}
-          </div>
-          <div className="font-overused text-[18px] leading-[20px] text-[#FFFFFF] font-medium ">
-            {name}
-          </div>
-        </div>
-        <div className="w-[124px] h-fit font-overused text-[14px] leading-[20px] text-[#BABABA] pb-5 ">
-          {skill}
-        </div>
-        <div className="flex gap-[10px] px-[12px] py-[7px] ">
-          <div className="px-[12px] py-[4px] rounded-[70px] border-[1px] border-[#2A2929] bg-[#333333] font-space text-[#C3C3C3] text-[12px] font-semibold ">
-            {button}
-          </div>
-          <div className="px-[12px] py-[4px] rounded-[70px] border-[1px] border-[#2A2929] bg-[#333333] font-space text-[#C3C3C3] text-[12px] font-semibold ">
-            {button1}
-          </div>
-          <div className="px-[12px] py-[4px] rounded-[70px] border-[1px] border-[#2A2929] bg-[#333333] font-space text-[#C3C3C3] text-[12px] font-semibold ">
-            {button2}
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <div className="font-space font-semibold text-[14px] text-[#BABABA] py-3 ">
-            {location}
-          </div>
-          <div className="flex gap-2 pb-5">
-            {" "}
-            <div>{svg}</div>
-            <div className="font-medium font-space leading-[20px] text-[14px] text-[#BDBDBD] w-[240px] h-[40px] ">
-              {locate}
+        <div className="border-[1px] rounded-[20px] bg-[#2A2929] border-[#333333] w-fit px-5 py-5 ">
+          <div className="flex items-center gap-3 py-5">
+            <div className="w-[48px] h-[48px] rounded-[10px] border-[1px] ">
+              <Image
+                height={500}
+                width={500}
+                className=" object-cover h-full w-full"
+                src={img}
+              />{" "}
+            </div>
+            <div className="font-overused text-[18px] leading-[20px] text-[#FFFFFF] font-medium ">
+              {name}
             </div>
           </div>
-        </div>
-        <div className="flex w-full justify-end items-end">
-        <div className="bg-[#8B66FE] py-[10px] px-[19px] rounded-[13px] w-[93px] h-[39px]">
-          <div className="font-space text-[15px] leading-[20px] font-medium text-[#FFFFFF] ">
-            {register}
+          <div className="w-[150px] h-fit font-overused text-[14px] leading-[20px] text-[#BABABA] pb-5 ">
+            {skill}
           </div>
-        </div>
-        </div>
+          <div className="flex gap-[10px] px-[12px] py-[7px] ">
+            <div className="px-[12px] py-[4px] rounded-[70px] border-[1px] border-[#2A2929] bg-[#333333] font-space text-[#C3C3C3] text-[12px] font-semibold ">
+              {button}
+            </div>
+            <div className="px-[12px] py-[4px] rounded-[70px] border-[1px] border-[#2A2929] bg-[#333333] font-space text-[#C3C3C3] text-[12px] font-semibold ">
+              {button1}
+            </div>
+            <div className="px-[12px] py-[4px] rounded-[70px] border-[1px] border-[#2A2929] bg-[#333333] font-space text-[#C3C3C3] text-[12px] font-semibold ">
+              {button2}
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="font-space font-semibold text-[14px] text-[#BABABA] py-3 ">
+              {location}
+            </div>
+            <div className="flex gap-2 pb-5">
+              {" "}
+              <div>{svg}</div>
+              <div className="font-medium font-space leading-[20px] text-[14px] text-[#BDBDBD] w-[240px] h-[40px] ">
+                {locate}
+              </div>
+            </div>
+          </div>
+          <div className="flex w-full justify-end items-end">
+            {eligible && (
+              <div className="bg-[#8B66FE] py-[10px] px-[19px] rounded-[13px] w-[93px] h-[39px]">
+                <div className="font-space text-[15px] leading-[20px] font-medium text-[#FFFFFF] ">
+                  Register
+                </div>
+              </div>
+            )}
+            {!eligible && (
+              <div className=" flex items-center py-[10px] px-[19px] rounded-[13px] w-full justify-end gap-3 h-[39px]">
+                <div>
+                  <No />
+                </div>
+                <div className="font-space text-[15px] leading-[20px] font-medium text-[#FD6666] ">
+                  Not eligible
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </Padding>
     </div>
